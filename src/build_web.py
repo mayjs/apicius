9e688df7ae303295a81f04462fbd9a0b8ae68df4
may <johannes.may@udo.edu>
2020-04-19 13:29:52 +0200

    Added index.html rendering

--- src/build_web.py
+++ src/build_web.py
@@ -2,28 +2,68 @@ from argparse import ArgumentParser
 from pathlib import Path
 import logging
 import json
-from recipe_parser import parse_and_render_recipe
+from recipe_parser import parse_and_render_recipe, html_template
+
+def render_table_row(title, ingredients, tags, url):
+    rendered_tags = '<ul class="tags-list">{}</ul>'.format(
+        "\n".join(f'<li class="tag">{tag}</li>' for tag in tags)
+    )
+    template = f"""
+        <tr>
+            <td><a href="{url}">{title}</a></td>
+            <td>{rendered_tags}</td>
+        </tr>
+    """
+    # <td>{", ".join(ingredients)}</td>
+    return template
 
 def build(source: Path, dest: Path):
+    logger = logging.getLogger("build")
     recipes_path = "recipes"
     recipe_out = dest / recipes_path
     recipe_out.mkdir(exist_ok=True, parents=True)
 
     recipes = []
+    table_rows = []
     for infile in source.iterdir():
         if infile.is_file():
+            logger.info(f"Handling {infile}")
             html_filename = infile.stem + ".html"
             outfile = recipe_out / html_filename
             title, ingredients, tags, html = parse_and_render_recipe(infile.read_text(), js_path_prefix="../", css_path_prefix="../")
             ingredient_names = list(set(name.lower() for name,_ in ingredients.keys()))
-            meta = {"modified": infile.stat().st_mtime, "title": title, "ingredients": ingredient_names, "url": f"{recipes_path}/{html_filename}"}
+            url = f"{recipes_path}/{html_filename}"
+            meta = {"modified": infile.stat().st_mtime, "title": title, "ingredients": ingredient_names, "url": url}
             recipes.append(meta)
+            table_rows.append(render_table_row(title, ingredient_names, tags, url))
             outfile.write_text(html)
     recipes = sorted(recipes, key=lambda meta: meta["modified"])
     with open(dest / "index.json", "w") as f:
         json.dump(recipes, f, indent=4)
 
+    nl = "\n"
+    body = f"""
+        <h2>Rezept-Übersicht:</h2>
+        <form class="search-form">
+            <label for="search-input">Suche:</label>
+            <input class="search-input" id="search-input" type="Suche" placeholder="Suche">
+            <button class="search-submit" onclick="search();" type="button">
+            Suchen
+            </button>
+        </form>
+        <table>
+        <tr>
+            <th>Name</th>
+            <th>Tags</th>
+        </tr>
+        {nl.join(table_rows)}
+    </table>
+    """
+    html = html_template.format(body=body, css_path_prefix="", js_path_prefix="")
+    (dest / "index.html").write_text(html)
+
 def main():
+    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     parser = ArgumentParser(description="Build a recipe website from a repository of markdown recipes")
     parser.add_argument("-s", "--source", help="The source directory containing markdown files", required=True)
