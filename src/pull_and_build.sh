1d66b5fb4f3546de978f474a8be11270dd1cf8f3
Johannes May <johannes@may-se.de>
2021-08-16 14:45:16 +0200

    Copy images to web

--- src/pull_and_build.sh
+++ src/pull_and_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # $1: src_path
 # $2: dest_path
 
@@ -7,3 +7,4 @@ cd $1
 git pull
 popd
 pipenv run python build_web.py -s "$1" -d "$2"
+rsync -rau "$1/../images" "web/"
